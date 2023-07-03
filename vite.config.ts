cd54cb5ef44a2dac675603e4936187d26d407405
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-03 22:46:33 +0200

    Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 6060,
-  },
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
-
   ],
   resolve: {
-    alias: [
-      { find: '/@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-      { find: '/IMG', replacement: fileURLToPath(new URL('./src/assets/images/', import.meta.url)) },
-    ],
-    extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
-  },
-  base: '/'
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
+
