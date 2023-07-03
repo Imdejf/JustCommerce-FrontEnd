0301d2e6fb3427b007b18d7e4241e81cc2bf4afb
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-03 22:42:11 +0200

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  server: {
+    port: 6060,
+  },
   plugins: [
     vue(),
     vueJsx(),
+
   ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    alias: [
+      { find: '/@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '/IMG', replacement: fileURLToPath(new URL('./src/assets/images/', import.meta.url)) },
+    ],
+    extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
+  },
+  base: '/'
 })
