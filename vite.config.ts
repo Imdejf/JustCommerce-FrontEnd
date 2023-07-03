086c9e3fcf883e2c25b57a58489446cfa1f34d56
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-03 22:50:47 +0200

    Added extension

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    extensions: ['.js','.ts','.json','.jsx','.mjs','.tsx','.vue']
   }
 })
 
