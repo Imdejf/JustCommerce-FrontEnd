deeecbf2d32feb4c3f79e45ea50ed2d36a54b3f8
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-07-04 11:10:41 +0200

    Modify package.json

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script lang="ts" setup>
-// import Cookies from 'universal-cookie'
-// import { onMounted } from 'vue'
-// import { RouterLink, RouterView } from 'vue-router'
-// import { useStoreStore } from './stores/store'
-// import { useLanguageStore } from './stores/language'
-// const cookies = new Cookies()
+import Cookies from 'universal-cookie'
+import { onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import { useStoreStore } from './stores/store'
+import { useLanguageStore } from './stores/language'
+const cookies = new Cookies()
 
-// const useStore = useStoreStore()
-// const language = useLanguageStore()
-// onMounted(async () => {
-//   const selectedStore = cookies.get('dsStore')
-//   if (selectedStore) {
-//     await language.setLanguages()
-//   }
-//   await useStore.setStores()
+const useStore = useStoreStore()
+const language = useLanguageStore()
+onMounted(async () => {
+  const selectedStore = cookies.get('dsStore')
+  if (selectedStore) {
+    await language.setLanguages()
+  }
+  await useStore.setStores()
 
-//   useStore.setCurrentStore(selectedStore)
-// })
+  useStore.setCurrentStore(selectedStore)
+})
 </script>
 
 <template>
   <header></header>
   <main>
     <a>Test</a>
-    <!-- <component :is="$route.meta.layout || 'div'">
+    <component :is="$route.meta.layout || 'div'">
       <router-view></router-view>
-    </component> -->
+    </component>
   </main>
 </template>
